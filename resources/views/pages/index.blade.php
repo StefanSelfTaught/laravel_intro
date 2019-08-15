3a981083e322ed871daca89ac727a6fda592bab5
Stefan <me@Stefan.com>
2019-08-15 17:47:52 +0300

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'LaravelIntro')}}</title>
+    </head>
+    <body>
+        <h1>Welcome to Laravel</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus itaque numquam est perspiciatis totam natus.</p>
+    </body>
+</html>
