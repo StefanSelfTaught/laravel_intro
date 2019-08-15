afe80dc7ed2c6baa360df2749e85a36b3a673e44
Stefan <me@Stefan.com>
2019-08-15 19:11:04 +0300

    Setup & Migrations

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'LaravelIntro')}}</title>
-    </head>
-    <body>
-        <h1>About</h1>
+@extends('layouts/app')
+
+@section('content')
+        <h1>{{$title}}</h1>
         <p>About page</p>
-    </body>
-</html>
+@endsection
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,13 +1,9 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'LaravelIntro')}}</title>
-    </head>
-    <body>
-        <h1>Welcome to Laravel</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus itaque numquam est perspiciatis totam natus.</p>
-    </body>
-</html>
+@extends('layouts/app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>Welcome To Laravel!</h1>
+        <p>This is the laravel application from the "Laravel From Scratch" YouTube series</p>
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+    </div>
+@endsection
